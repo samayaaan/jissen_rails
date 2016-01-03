f87ddcc0d9f3657cd25e60b793d6cb7ca47f531a
samayaaan <samayaaan@gmail.com>
2016-01-03 17:46:01 +0900

    ジェネレータ周りの設定変えてみる

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,13 @@ module Baukis
     config.i18n.load_path +=
       Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controllers_specs false
+      g.view_specs false
+    end
   end
 end
